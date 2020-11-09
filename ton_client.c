0936a1ebdc022d676fb8c6664c853c0c1601ab95
Andrew Anisimov <anisimov.new@gmail.com>
2020-11-09 22:59:54 +0300

    Remove callback code
    It's useless.

--- ton_client.c
+++ ton_client.c
@@ -21,101 +21,6 @@
 	ZEND_PARSE_PARAMETERS_END()
 #endif
 
-void fail(const char *fmt, ...) {
-    va_list args;
-            va_start(args, fmt);
-    vfprintf(stderr, fmt, args);
-            va_end(args);
-    exit(-1);
-}
-
-typedef struct ton_handler_data {
-    zval handler;
-    zend_fcall_info_cache fcc;
-} ton_handler_data_t;
-
-ton_handler_data_t *create_handler_data(zval * handler) {
-    ton_handler_data_t *data = calloc(1, sizeof(ton_handler_data_t));
-    data->fcc = empty_fcall_info_cache;
-    ZVAL_COPY(&data->handler, handler);
-    return data;
-}
-
-void free_handler_data(ton_handler_data_t *data) {
-    assert(data);
-    if (!data) {
-        fail("NULL passed to free_handler_data\n");
-        return;
-    }
-    free(data);
-}
-
-char *mem_copy_str_n(const char *str, uint32_t len) {
-    char *copy = malloc((len + 1) * sizeof(char));
-    strncpy(copy, str, len);
-    copy[len] = 0;
-    return copy;
-}
-
-void ton_response_handler(
-        void *request_ptr,
-        tc_string_data_t params_json,
-        uint32_t response_type,
-        bool finished) {
-
-#if defined(TON_DEBUG)
-    char* json = mem_copy_str_n(params_json.content, params_json.len);
-    fprintf(stderr, "ton_response_handler is called: response_type=%d; finished=%d; params_json=%s\n",
-            response_type, finished, json);
-    free(json);
-#endif
-
-    ton_handler_data_t *data = request_ptr;
-    assert(data);
-
-    if (!data) {
-        fail("NULL ptr passed to response handler\n");
-        return;
-    }
-
-#if defined(TON_DEBUG)
-    fprintf(stderr, "tmp ref count is: %d", Z_REFCOUNT(data->handler));
-    fprintf(stderr, "calling callback handler: ");
-    zend_string	*callback_name = zend_get_callable_name(&data->handler);
-    fprintf(stderr, "%s\n", ZSTR_VAL(callback_name));
-    zend_string_release_ex(callback_name, 0);
-#endif
-
-    zval callback_args[3]; // json, response_type, finished
-    ZVAL_STRINGL(&callback_args[0], params_json.content, params_json.len);
-    ZVAL_LONG(&callback_args[1], (zend_long)response_type);
-    ZVAL_BOOL(&callback_args[2], (zend_bool)finished);
-
-    zend_fcall_info fci;
-    fci.size = sizeof(fci);
-    fci.object = NULL;
-    fci.param_count = 3; // json, response_type, finished
-    fci.params = callback_args;
-    fci.no_separation = 0;
-    ZVAL_COPY_VALUE(&fci.function_name, &data->handler);
-    int error = zend_call_function(&fci, &data->fcc);
-    if (error == FAILURE) {
-#if defined(TON_DEBUG)
-        fprintf(stderr, "callback failed\n");
-#endif
-        php_error_docref(NULL, E_WARNING, "Cannot call the callback");
-    } else if (error == SUCCESS) {
-#if defined(TON_DEBUG)
-        fprintf(stderr, "callback called successfully\n");
-#endif
-    }
-    zval_ptr_dtor(&callback_args[0]);
-
-    if (finished) {
-        free_handler_data(data);
-    }
-}
-
 /* {{{ string ton_create_context( string $config_json )
  */
 PHP_FUNCTION(ton_create_context)
@@ -171,54 +76,32 @@ PHP_FUNCTION(ton_destroy_context)
 }
 /* }}} */
 
-/* {{{ void ton_request( int $context, string $function_name, string $params_json, mixed callback )
+/* {{{ void ton_request( int $context, string $function_name, string $params_json )
  */
 PHP_FUNCTION(ton_request)
 {
     zend_long context;
     zend_string *function_name;
     zend_string *params_json;
-    zval *callback;
-    zend_fcall_info_cache fcc;
 
 #if defined(TON_DEBUG)
     fprintf(stderr, "ton_request is called\n");
 #endif
 
-    ZEND_PARSE_PARAMETERS_START(4, 4)
+    ZEND_PARSE_PARAMETERS_START(3, 3)
         Z_PARAM_LONG(context)
         Z_PARAM_STR(function_name)
         Z_PARAM_STR(params_json)
-        Z_PARAM_ZVAL(callback)
     ZEND_PARSE_PARAMETERS_END();
 
-    if (!zend_is_callable_ex(callback, NULL, 0, NULL, &fcc, NULL)) {
-        zend_string	*callback_name = zend_get_callable_name(callback);
-        php_error_docref(NULL, E_WARNING, "Requires argument 2, '%s', to be a valid callback", ZSTR_VAL(callback_name));
-        zend_string_release_ex(callback_name, 0);
-        return;
-    }
-
-#if defined(TON_DEBUG)
-    zend_string	*callback_name = zend_get_callable_name(callback);
-    fprintf(stderr, "context id: %d, func name: %s; params: %s; callback name: %s\n",
-        (int)context, ZSTR_VAL(function_name), ZSTR_VAL(params_json), ZSTR_VAL(callback_name));
-    zend_string_release_ex(callback_name, 0);
-#endif
-
-    ton_handler_data_t *data = create_handler_data(callback);
     tc_string_data_t f_name = {ZSTR_VAL(function_name), ZSTR_LEN(function_name)};
     tc_string_data_t f_params = {ZSTR_VAL(params_json), ZSTR_LEN(params_json)};
-
-// NOTE: seems it's impossible to share callback function between multiple threads,
-// since Zend uses thread locals internally (share nothing memory management model).
-// So we're just using sync version here, which seems the only one option for us.
-//    tc_request_ptr(context, f_name, f_params, data, &ton_response_handler);
-
     tc_string_handle_t * response_handle = tc_request_sync(context, f_name, f_params);
     tc_string_data_t json = tc_read_string(response_handle);
-    ton_response_handler(data, json, 0, true);
+    zend_string *response_json = zend_string_init(json.content, json.len, 0);
     tc_destroy_string(response_handle);
+
+    RETURN_STR(response_json);
 }
 /* }}}*/
 
@@ -254,11 +137,10 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_ton_destroy_context, 0, 0, 1)
     ZEND_ARG_INFO(0, context)
 ZEND_END_ARG_INFO()
 
-ZEND_BEGIN_ARG_INFO_EX(arginfo_ton_request, 0, 0, 4)
+ZEND_BEGIN_ARG_INFO_EX(arginfo_ton_request, 0, 0, 3)
     ZEND_ARG_INFO(0, context)
     ZEND_ARG_INFO(0, function_name)
     ZEND_ARG_INFO(0, params_json)
-    ZEND_ARG_INFO(0, callback)
 ZEND_END_ARG_INFO()
 /* }}} */
 
