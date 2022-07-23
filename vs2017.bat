c84f777f9fef1ff759b59fe38663d1e7944d37b6
Andrew Anisimov <anisimov.new@gmail.com>
2022-07-23 21:34:43 +0300

    Cleanup.

--- vs2017.bat
+++ vs2017.bat
@@ -5,7 +5,6 @@ choco install --yes --force visualstudio2017buildtools visualstudio2017-workload
 echo DONE Installing Visual Studio 2017 Build Tools
 
 echo Setting up env variables
-dir /s/b "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools"
 if "%3" == "x64" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 if "%3" == "x86" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars32.bat"
 if not "%errorlevel%" == "0" exit /b %errorlevel%
